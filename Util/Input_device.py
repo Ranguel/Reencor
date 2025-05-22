6c0f5bf6466d2f74c498ef597a17e53826d781f4
Humberto <hldtux@gmail.com>
2025-05-22 02:19:57 -0300

    add ps4 joystick

--- Util/Input_device.py
+++ Util/Input_device.py
@@ -45,6 +45,17 @@ joystick_name_mapping = {
         (("a", 2),),
         (("a", 3),),
     ),
+    "Sony Interactive Entertainment Wireless Controller": (
+        (("a", 0),),
+        (("a", 1),),
+        (("b", 4),),
+        (("b", 3),),
+        (("b", 2),),
+        (("b", 5),),
+        (("b", 1),),
+        (("a", 2),),
+        (("a", 3),),
+    ),
 }
 
 
